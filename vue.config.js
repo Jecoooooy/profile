d5f9ebbe78b7db3b3357f0bb3852aa658e7061f5
Jerico <bencitojerico@gmail.com>
2024-08-15 06:35:46 +0800

    asd

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,12 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+// import { defineConfig} from 'vite'
+// import vue from '@vitejs/plugin-vue'
+
+// export default defineConfig({
+//   plugins:  [
+//     vue()
+//   ]
+// })
