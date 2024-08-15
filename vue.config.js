cf7509f3b57139a0c61112022e85fb9e8f7b14ab
Jerico <bencitojerico@gmail.com>
2024-08-15 22:56:51 +0800

    try

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/<profile>/' // Replace with your repository name
+    ? '/<profile>/' 
     : '/'
-  // transpileDependencies: true
 })
-
-// import { defineConfig} from 'vite'
-// import vue from '@vitejs/plugin-vue'
-
-// export default defineConfig({
-//   plugins:  [
-//     vue()
-//   ]
-// })
